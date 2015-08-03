46908108a7d3e3dabb05bf9bf662ee211ed7c815
Subby Olubeko <subyolubeko@yahoo.com>
2015-08-02 23:43:44 -0400

    New pages
    Contact and publications pages added

---
+++ contact.php
@@ -0,0 +1,20 @@
+<div class="row">
+      <div class="columns large-12">
+        <h1>Contact Us</h1>
+        <h2>Email</h2>
+ 			<p>
+            Kimberle Koile: <a href="mailto:kkoile@csail.mit.edu">kkoile@csail.mit.edu</a><br>
+            Click <a href="index.php?p=about#team">here</a> to contact other team members.
+            </p>
+            
+        <h2>Postal Mail</h2>
+	 		<p>
+            <address>ATTN: Kimberle Koile<br>32 Vassar Street, MIT CSAIL Lab at the Stata Center, Bldg 32<br>Office 32-221<br>Cambridge, MA 02139</address>
+            </p>
+
+        <h2>Phone</h2>
+ 			<p>
+            Call us at: <a href="tel:16172536037">(617) 253-6037</a>
+            </p>
+      </div>
+</div>
---
+++ publications.php
@@ -0,0 +1,72 @@
+<div class="row">
+      <div class="columns large-12">
+        <h1>Publications</h1>
+      </div>
+       	<a name="conference"></a>
+        <p>
+        <!-- Spacer -->
+        <br>&nbsp;<br>
+        </p>
+		<h2>Conference Papers</h2>
+ 			<p class="publication">Koile, K., Chevalier, K., Low, C., Pal, S., Rogal, A., Singer, D., Sorensen, J., Tay, K.S., Wu, K. Supporting Pen-Based Classroom Interaction: New Findings and Functionality for Classroom Learning Partner.  In the <i>First International Workshop on Pen-Based Learning Technologies</i>, May 24-26, 2007.<br>
+            <a href="documents/KoilePLT2007.pdf"><img src="images/acroread.gif" border="0"> </a></p>
+			<p class="publication">Koile, K., Chevalier, K., Rbeiz, M., Rogal, A., Singer, D., Sorensen, J., Smith, A., Tay, K.S., Wu, K. Supporting Feedback and Assessment of Digital Ink Answers to In-Class Exercises. In the <i>Proceedings of the Nineteenth Conference on Innovative Applications of AI</i>, July 22-29, 2007.<br>
+            <a href="documents/KoileIAAI07.pdf"><img src="images/acroread.gif" border="0" alt="" width="20" height="20"> </a></p>
+ 			<p class="publication">Koile, K. and Singer, D.	Improving Learning in CS1 via Tablet-PC-based In-Class Assessment. In <i>Proceedings of ICER 2006</i> (Second International Computing Education Research Workshop), September 9-10, 2006, University of Kent, Canterbury, UK.<br>
+ 			<a href="documents/KoileSingerICER06.pdf"><img src="images/acroread.gif" border="0"> </a></p>
+            <p class="publication">Koile, K. and Singer, D., Development of a Tablet-PC-based System to Increase Instructor-Student Classroom Interactions and Student Learning. &nbsp;In<i> The Impact of Pen-based Technology on Education; Vignettes, Evaluations, and Future Directions.</i> Berque, D., Gray, J., and Reed, R. (editors). Purdue University Press, 2006.<br>
+			<a href="documents/KoileSinger06.pdf"><img src="images/acroread.gif" border="0" alt="" width="20" height="20"> </a></p>
+			<p class="publication">Koile, K. and Singer, D., Assessing the Impact of&nbsp; a Tablet-PC-based Classroom Interaction System. &nbsp;To appear in&nbsp;<i> Proceedings of Workshop on the Impact of Pen-Based Technology on Education (WIPTE) 2008.&nbsp;</i> Berque, D., Gray, J., and Reed, R. (editors). Purdue University Press, 2008.<br>
+			<a href="documents/KoileSingerWIPTE08.pdf"><img src="images/acroread.gif" border="0" alt="" width="20" height="20"> </a></p>
+			<p class="publication">Tay, K.S. and Koile, K., Improving Digital Ink Interpretation through Expected Type Prediction and Dynamic Dispatch, In <i>Proceedings of the 19th International Conference on Pattern Recognition</i>, December 8-11, 2008.<br>
+			<a href="documents/TayKoileICPR08.pdf"><img src="images/acroread.gif" border="0"> </a></p>
+                
+		<a name="presentations"></a>
+		<p>
+        <!-- Spacer -->
+        <br>&nbsp;<br>
+        <br>&nbsp;<br>
+        <br>&nbsp;<br>
+        </p>
+        <h2>Presentations</h2>
+ 		<p class="publication"> <br>
+        <a href="documents/hvd-poster-kk-ds.ppt"><img src="images/powerpoint.gif" border="0"> </a></p>
+		<p><a href="#top"><img src="../images/arrow-top.jpg">Top</a> </p>
+        
+        <a name="posters"></a>
+		<p>
+        <!-- Spacer -->
+        <br>&nbsp;<br>
+        <br>&nbsp;<br>
+        <br>&nbsp;<br>
+        </p>
+        <h2>Posters</h2>
+ 		<p class="publication">Koile, K., Singer, D., Improving Learning via Tablet-PC-based  In-Class Assessment, <i>Symposium on Technology in Undergraduate Science Education</i>, Harvard University, June 15-16, 2006, <a href="http://www.lifescience.fas.harvard.edu/tech/">http://www.lifescience.fas.harvard.edu/tech/</a> <br>
+		<a href="documents/hvd-poster-kk-ds.pdf"><img src="images/acroread.gif" border="0"> </a></p>
+ 		<p class="publication">Koile, K., Classroom Learning Partner: Supporting In-Class Assessment in Large Classes, <i>Symposium on Technology in Undergraduate Science Education</i>, Harvard University, June 15-16, 2006,
+        <a href="http://www.lifescience.fas.harvard.edu/tech/">http://www.lifescience.fas.harvard.edu/tech/</a>
+        <a href="documents/hvd-poster-clp.pdf"><br> <img src="images/acroread.gif" border="0"> </a></p>
+ 		<p><a href="#top"><img src="../images/arrow-top.jpg">Top</a> </p>
+        
+		<a name="theses"></a>
+		<p>
+        <!-- Spacer -->
+        <br>&nbsp;<br>
+        <br>&nbsp;<br>
+        <br>&nbsp;<br>
+        </p>
+        <h2>Theses</h2>
+ 		<p class="publication">Rbeiz, M., "Semantic Representation of Digital Ink in the Classroom Learning Partner", Massachusetts Institute of Technology MEng Thesis, May 2006. <br>
+        <a href="documents/Michel.Rbeiz.Thesis.pdf"><img src="images/acroread.gif"></a></p>
+        <p class="publication">Smith, A., "Aggregation of Student Answers in a Classroom Setting", Massachusetts Institute of Technology MEng Thesis, August 2006. <br>
+        <a href="documents/Amanda.Smith.Thesis.pdf"><img src="images/acroread.gif"></a></p>
+        <p class="publication">Iancu, K., "Security for Classroom Learning Partner", Massachusetts Institute of Technology MEng Thesis, September 2006. <br>
+        <a href="documents/Karin.Iancu.Thesis.pdf"><img src="images/acroread.gif" alt="" width="20" height="20"></a><br></p>
+        <p class="publication">Chevalier, K., "Interpretation of Box-and-Pointer Diagrams in Classroom Learning Partner", Massachusetts Institute of Technology MEng Thesis, May 2007. <br>
+        <a href="documents/Kevin.Chevalier.Thesis.pdf"><img src="images/acroread.gif"></a></p>
+        <p class="publication">Wu, K., "Interpretation and Aggregation of Marks in Classroom Learning Partner", Massachusetts Institute of Technology MEng Thesis, January 2008. <br>
+        <a href="documents/Kenneth.Wu.Thesis.pdf"><img src="images/acroread.gif"></a></p>
+        <p class="publication">Tay, K.S., "Improving Digital Ink Interpretation through Expected Type Prediction and Dynamic Dispatch", Massachusetts Institute of Technology MEng Thesis, May 2008. <br>
+        <a href="documents/KahSeng.Tay.Thesis.pdf"><img src="images/acroread.gif"></a></p>
+		<p><a href="#top"><img src="../images/arrow-top.jpg">Top</a> </p>
+</div>
